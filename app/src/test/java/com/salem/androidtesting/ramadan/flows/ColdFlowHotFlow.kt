932fc71789a890297c1f78295387327cc7ec852a
mhmd salem <mhmd.salem33@yahoo.com>
2025-07-25 16:36:19 +0300

    convert cold flow to hot flow

--- app/src/test/java/com/salem/androidtesting/ramadan/flows/ColdFlowHotFlow.kt
+++ app/src/test/java/com/salem/androidtesting/ramadan/flows/ColdFlowHotFlow.kt
@@ -1,11 +1,16 @@
 package com.salem.androidtesting.ramadan.flows
 
 import app.cash.turbine.test
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.runTest
 import org.junit.Test
 import org.amshove.kluent.shouldBeEqualTo
+import kotlin.test.assertEquals
 
 
 class ColdFlow {
@@ -19,5 +24,14 @@ class ColdFlow {
             awaitItem() shouldBeEqualTo  30
             awaitComplete()
         }
+    }
+
+    // with hot flow we don't use a awaitComplete()
+
+    @Test
+    fun `convert cold flow to hot flow`() = runTest {
+        val flow = flowOf(1,2,3).map { it * 10 } .stateIn(this)
+
+        flow.test { awaitItem() shouldBeEqualTo 30 }
     }
 }
