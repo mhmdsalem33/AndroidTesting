7370e5583897bf4f25911fda5bc59a2bcbb25981
mhmd salem <mhmd.salem33@yahoo.com>
2025-07-27 21:08:19 +0300

    test shared flow with SharingStarted Eagerly

--- app/src/test/java/com/salem/androidtesting/ramadan/flows/HotFlow.kt
+++ app/src/test/java/com/salem/androidtesting/ramadan/flows/HotFlow.kt
@@ -1,18 +1,22 @@
 package com.salem.androidtesting.ramadan.flows
 
 import app.cash.turbine.test
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.cancelChildren
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.flow.shareIn
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.runTest
 import org.amshove.kluent.shouldBe
 import org.amshove.kluent.shouldBeEqualTo
 import org.junit.Test
 
+@OptIn(ExperimentalCoroutinesApi::class)
 class HotFlow {
 
     @Test
@@ -94,7 +98,36 @@ class HotFlow {
             awaitItem() shouldBeEqualTo "Event 3"
         }
         coroutineContext.cancelChildren()
+
     }
 
+
+        @Test
+        fun `collect with eager strategy`() = runTest( context = UnconfinedTestDispatcher() ) {
+
+            val flow = flowOf(
+                "Event 1",
+                "Event 2",
+                "Event 3"
+            )
+
+            val sharedFlow = flow
+                .onCompletion {
+                    println("Shared flow completed")
+                }.shareIn(
+                    scope   =  this,
+                    started = SharingStarted.Eagerly ,
+                    replay  = 1
+                )
+
+            // SharingStarted.Lazily
+            // ✅ 🚀 The flow starts immediately, even if no one is listening.
+            // ❌ It does not wait for a listener to start.
+
+            sharedFlow.test {
+                awaitItem() shouldBeEqualTo "Event 3"
+            }
+            coroutineContext.cancelChildren()
+        }
 
 }
